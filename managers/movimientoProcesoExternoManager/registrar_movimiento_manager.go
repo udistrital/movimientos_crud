e3ccf81137000ca89dd4dd04ca01e279e896ac8f
Juan Camilo Sarmiento Reyes <jcsarmientor@correo.udistrital.edu.co>
2019-07-01 21:52:25 -0500

    Función para registrar movimientos de tipo adición Se creo un manager para manejar la transacción de registro en movimiento_proceso_externo y movimiento_detalle issue #12

---
+++ managers/movimientoProcesoExternoManager/registrar_movimiento_manager.go
@@ -0,0 +1,25 @@
+package movimientoProcesoExternoManager
+
+import (
+	"github.com/astaxie/beego/orm"
+	"github.com/udistrital/movimientos_crud/models"
+)
+
+func RegistrarMovimientoProcesoExterno(movimientoProcesoExterno *models.MovimientoProcesoExterno, movimientoDetalle *models.MovimientoDetalle) {
+	o := orm.NewOrm()
+	err := o.Begin()
+
+	id, err := o.Insert(movimientoProcesoExterno)
+	if err != nil {
+		o.Rollback()
+		panic(err) 
+	}
+	movimientoProcesoExterno.Id = int(id)
+	movimientoDetalle.MovimientoProcesoExternoId = movimientoProcesoExterno
+	_, err = o.Insert(movimientoDetalle)
+	if err != nil {
+		o.Rollback()
+		panic(err)
+	}
+	o.Commit()
+}
