99d2aa2df76d63b2a44a3d3485bfa5120218993e
Juan Camilo Sarmiento Reyes <jcamilosarmientor@gmail.com>
2019-07-06 22:00:22 -0500

    Transacción para registros multiples de: movimiento_proceso_externo movimiento_detalle
    issue #21

---
+++ managers/movimientoDetalleManager/movimientoDetalleManager.go
@@ -0,0 +1,35 @@
+package movimientodetallemanager
+
+import (
+	"log"
+
+	"github.com/astaxie/beego/orm"
+	"github.com/udistrital/movimientos_crud/models"
+)
+
+// RegistrarMultipleManager realiza multiples registros en una transacción sobre las tablas:
+// movimiento_proceso_externo y movimiento_detalle
+func RegistrarMultipleManager(movimientosDetalle []*models.MovimientoDetalle) (idRegistrados []int64) {
+	o := orm.NewOrm()
+
+	if err := o.Begin(); err == nil {
+		for _, movimientoDetalle := range movimientosDetalle {
+			id, err := o.Insert(movimientoDetalle.MovimientoProcesoExternoId)
+			if err != nil {
+				o.Rollback()
+				log.Panicln(err.Error())
+			}
+
+			movimientoDetalle.MovimientoProcesoExternoId.Id = int(id)
+
+			id, err = o.Insert(movimientoDetalle)
+			if err != nil {
+				o.Rollback()
+				log.Panicln(err.Error())
+			}
+
+			idRegistrados = append(idRegistrados, id)
+		}
+	}
+	return
+}
