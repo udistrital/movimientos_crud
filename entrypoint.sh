7d7eef72afd83ac609dbd7dba9e4b46b81878517
Soulfilip <filip9322@gmail.com>
2019-10-26 23:10:37 -0500

    Se corrigen archivos de configuracion para despliegue

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ set -o pipefail
 
 if [ -n "${PARAMETER_STORE:-}" ]; then
   export MOVIMIENTOS_CRUD_DB_USER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/movimientos_crud/db/username --output text --query Parameter.Value)"
-  export MOVIMIENTOS_CRUD_DB_PASSWORD="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/movimientos_crud/db/password --output text --query Parameter.Value)"
+  export MOVIMIENTOS_CRUD_DB_PASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/movimientos_crud/db/password --output text --query Parameter.Value)"
 fi
 
 exec ./main "$@"
