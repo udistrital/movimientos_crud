258ae99ff90d2333c615e3f6256c05d7579ca5d2
Alex Bustos <alexfbp@gmail.com>
2021-02-12 11:45:48 -0500

    feat: insert movimientos arka (ver mensaje del commit -->)
    Relacionado con udistrital/arka_cliente#482
    
    Movimientos contables por:
    
    - Traslado de bienes y servicios...
    - Bajas de bienes y servicios...
    
    ... generadas por el Sistema de Almacen e Inventarios Arka (II)

---
+++ database/files/20210212_095043_insertArkaBajasTraslados_up.sql
@@ -0,0 +1,4 @@
+INSERT INTO movimientos.tipo_movimiento (acronimo, nombre, descripcion, activo)
+VALUES
+('b_arka', 'Baja', 'Baja de bienes y servicios de Arka II', true),
+('tr_arka', 'Traslado', 'Traslado de bienes y servicios de Arka II',  true);
