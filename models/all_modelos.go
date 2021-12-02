48f9a68cce3934a813b031cbfc992c8aef011b0f
brayanpasa99 <brayanpasa99@hotmail.com>
2021-12-01 23:58:54 -0500

    feat: Actualizacion modelos

--- models/all_modelos.go
+++ models/all_modelos.go
@@ -3,10 +3,17 @@ package models
 type CuentasMovimientoProcesoExterno struct {
 	Cuen_Pre     string
 	Mov_Proc_Ext string
+	Saldo        float64
+	Valor        float64
 }
 
 type Cuen_Pre struct {
 	RubroId                string
 	FuenteFinanciamientoId string
 	ActividadId            string
+}
+
+type mov_proc_ext_con_hijos struct {
+	MovimientoProcesoExterno
+	Hijos []MovimientoDetalle
 }
