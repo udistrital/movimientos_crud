8a91a28b485611e1655745301ae3c2cc616452f8
Daniel Aguazaco <daaguazacot@ucentral.edu.co>
2022-07-10 23:37:24 -0500

    fix: restauracion de detalle en modelo MovProcExt

--- models/movimiento_proceso_externo.go
+++ models/movimiento_proceso_externo.go
@@ -18,6 +18,7 @@ type MovimientoProcesoExterno struct {
 	Activo                   bool            `orm:"column(activo);null"`
 	FechaCreacion            time.Time       `orm:"auto_now_add;column(fecha_creacion);null"`
 	FechaModificacion        time.Time       `orm:"auto_now;column(fecha_modificacion);null"`
+	Detalle                  string          `orm:"column(detalle);type(jsonb);null"`
 }
 
 func (t *MovimientoProcesoExterno) TableName() string {
