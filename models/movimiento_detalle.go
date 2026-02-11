10c858a50e85ccd241b1d31cc33337ad3e2b7f58
Jhon Gabriel Castellanos Jimenez <corio27@gmail.com>
2026-02-11 13:28:01 -0500

    Fix dot-notation replacement in query filtering
    se ajusta función de string.Replace

--- models/movimiento_detalle.go
+++ models/movimiento_detalle.go
@@ -58,7 +58,7 @@ func GetAllMovimientoDetalle(query map[string]string, fields []string, sortby []
 	// query k=v
 	for k, v := range query {
 		// rewrite dot-notation to Object__Attribute
-		k = strings.Replace(k, ".", "__", -1)
+		k = strings.Replace(k, "__", ".", -1)
 		if strings.Contains(k, "isnull") {
 			qs = qs.Filter(k, (v == "true" || v == "1"))
 		} else {
